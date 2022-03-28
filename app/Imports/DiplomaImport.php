05824047f27d7846894c4a746b3967da983ae9ff
Ronaldo Neves <ronaldo@uema.br>
2022-03-28 12:00:10 -0300

    finalizando xls diploma

--- app/Imports/DiplomaImport.php
+++ app/Imports/DiplomaImport.php
@@ -15,12 +15,12 @@ class DiplomaImport implements ToModel
     public function model(array $row)
     {
         return new Diploma([
-            'nome' => $row[0],
-            'codigo' => $row[1],
-            'cpf' => str_pad($row[2] , 11 , '0' , STR_PAD_LEFT),
-            'curso' => $row[2],
-            'centro' => $row[3],
-            'data' => $row[5],
+            'nome' => trim($row[0]),
+            'codigo' => trim($row[1]),
+            'cpf' => str_pad(trim($row[2]), 11 , '0' , STR_PAD_LEFT),
+            'curso' => trim($row[3]),
+            'centro' => trim($row[4]),
+            'data' => trim($row[5]),
             'hash' => Hash::make($row[2]), //PEGAR O ID DO CPF
             'termo' => 1
         ]);
